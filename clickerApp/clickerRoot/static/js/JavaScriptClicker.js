8bb3346a97323e28cef9ac72504f9ac530f71105
PointDisarray <lightscape@protonmail.com>
2018-11-26 20:34:20 +0100

    Init

--- clickerApp/clickerRoot/static/js/JavaScriptClicker.js
+++ clickerApp/clickerRoot/static/js/JavaScriptClicker.js
@@ -1,11 +1,30 @@
 var clickerNumber=0
+var controller=1
 
 function incrementation() {
-    clickerNumber++;
-    if(clickerNumber == 5) {
-        $('#h2counter').text("https://www.youtube.com/watch?v=ylbmc1hAofg");
+    if(controller==1) {
+        clickerNumber++;
+        $('#h2counter').text('total: ' + clickerNumber);
+        if(clickerNumber == 5) {
+            $('#pressHref').text("PRESS");
+            document.getElementById("pressHref").href="https://www.youtube.com/watch?v=ylbmc1hAofg";
+            //$('#h2counter').text('');
+            controller = 0;
+        }
     }
-    else{
-     $('#h2counter').text('count: ' + clickerNumber);
+}
+
+function collapse() {
+    if(document.getElementById("mynavbar").aria-expanded == "false") {
+        //document.getElementById("mynavbar").class="navbar-collapse collapse in";
+        //document.getElementById("mynavbar").aria-expanded="true";
+        $('#mynavbar').class="navbar-collapse collapse in";
+        $('#mynavbar').aria-expanded="true";
+    }
+    else {
+        //document.getElementById("mynavbar").class="navbar-collapse collapse";
+        //document.getElementById("mynavbar").aria-expanded="false";
+        $('#mynavbar').class="navbar-collapse collapse";
+        $('#mynavbar').aria-expanded="false";
     }
 }
