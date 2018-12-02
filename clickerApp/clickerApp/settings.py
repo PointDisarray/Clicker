a00d11233fcdf1bd9912fb86e3efc51696f0e007
PointDisarray <lightscape@protonmail.com>
2018-12-02 13:58:31 +0100

    Init

--- clickerApp/clickerApp/settings.py
+++ clickerApp/clickerApp/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'clickerRoot.apps.ClickerrootConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clickerRoot',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'clickerApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Clicker',
+        'USER': 'postgres',
+        'PASSWORD': 'a',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- clickerApp/clickerRoot/views.py
+++ clickerApp/clickerRoot/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 
 def index(request):
-    return render(request, "index.html")
+    user = User.objects.all().first()
+    if not user:
+        return render(request, "index.html")
+    else:
+        return render(request, "index.html", {'counter': user.counter})
 
 
 def reee(request):
