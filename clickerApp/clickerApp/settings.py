49e3fbb51cb67eca04d46e223c254791138c8674
IvanHavr <henkok211@gmail.com>
2018-12-09 23:02:20 +0100

    the next one

--- clickerApp/clickerApp/settings.py
+++ clickerApp/clickerApp/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -141,4 +140,4 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
