bcf8211c01a39e6e2497404b8cf4d042c534c114
IvanHavr <henkok211@gmail.com>
2018-12-09 21:30:23 +0100

    the next one

--- clickerApp/clickerApp/settings.py
+++ clickerApp/clickerApp/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'clickerApp/clickerRoot/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
