bd10c378e797c57126b4c41b39c08322162d832f
IvanHavr <henkok211@gmail.com>
2018-12-09 22:25:09 +0100

    the next one

--- clickerApp/clickerApp/settings.py
+++ clickerApp/clickerApp/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -130,11 +130,11 @@ USE_TZ = True
 # static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/clickerRoot/static'),
-    '/static'
+    os.path.join(BASE_DIR, 'static'),
+    '/static/'
 )
