c3ffcd0cc527816bebfd3f71aadcb0893ca39050
IvanHavr <henkok211@gmail.com>
2018-12-12 23:27:13 +0100

    update

--- clickerApp/clickerRoot/src/socket_server_side.py
+++ clickerApp/clickerRoot/src/socket_server_side.py
@@ -7,7 +7,7 @@ from ..models import User
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('djangoclickers.herokuapp.com', 9005)
+server_address = ('https://djangoclickers.herokuapp.com', 9005)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
